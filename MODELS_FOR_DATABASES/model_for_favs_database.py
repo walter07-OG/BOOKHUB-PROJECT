3c2f083f6dcec8c5a0d5541e264ca96be032dfe5
$_SUDOcode_44 <kwamecode11@gmail.com>
2025-06-27 02:43:23 -0700

    created two directories to separate the enpoints for the USER functions from the FAVOURITES functions.

--- MODELS_FOR_DATABASES/model_for_favs_database.py
+++ MODELS_FOR_DATABASES/model_for_favs_database.py
@@ -1,7 +1,6 @@
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import declarative_base, relationship
 from DATABASES import favourite_book_database
 from sqlalchemy import Column, String, JSON
-from typing import List
     
 '''We design the model of the Table we want to use for storing information
 about a book.(We add the features a book has, and which we will need in the future,
@@ -9,12 +8,15 @@ for data retrieval)'''
 
 Base = declarative_base()
 
-class FAVOURITE_BOOK(Base):
+class FAVOURITE_BOOKS(Base):
     __tablename__ =  "USER_FAVOURITES"
     '''NOTE: If two hashed passwords cannot be the same,  then we will use the hashed passwords 
     as the primary key.'''
     user_password = Column(String, primary_key = True, index = True)
+    
     user_favourites = Column(JSON, index = True)
+
+    books_owner = relationship("USERS", back_populates = "user_favourites")
 
 '''let's create the table, using metadata'''
 Base.metadata.create_all(bind = favourite_book_database.the_engine)
--- MODELS_FOR_DATABASES/model_for_users_database.py
+++ MODELS_FOR_DATABASES/model_for_users_database.py
@@ -1,6 +1,7 @@
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import declarative_base, relationship
 from DATABASES import book_hub_users_database
-from sqlalchemy import Column, String, INTEGER, FLOAT
+from sqlalchemy import Column, String, ForeignKey
+from model_for_book_database import Book
     
 '''We design the model of the Table we want to use for storing information
 about a book.(We add the features a book has, and which we will need in the future,
@@ -10,12 +11,10 @@ Base = declarative_base()
 
 class USERS(Base):
     __tablename__ =  "USERS"
+    hashed_password = Column(String, ForeignKey("USER_FAVOURITES.user_password"), primary_key = True)
     username = Column(String, index = True)
-    user_id = Column(INTEGER, primary_key = True, index = True)
     email = Column(String, index = True) 
-    hashed_password = Column(String, index = True)
-    favourites:list =  []
-
+    user_favourites = relationship("FAVOURITE_BOOKS", back_populates = "user_favourites")
 
 '''let's create the table, using metadata'''
 Base.metadata.create_all(bind = book_hub_users_database.the_engine)
