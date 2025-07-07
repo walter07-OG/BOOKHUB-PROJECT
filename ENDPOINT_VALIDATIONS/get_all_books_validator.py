b9dfc801ee3e0054f8f45e53449ad747f3109585
$_SUDOcode_44 <kwamecode11@gmail.com>
2025-07-07 16:22:02 -0700

    Sorry! the api_key field was wrong. I thought that was a class model for the request. It was rather the class model for the response!

--- ENDPOINT_VALIDATIONS/get_all_books_validator.py
+++ ENDPOINT_VALIDATIONS/get_all_books_validator.py
@@ -5,6 +5,5 @@ from typing import List
 '''The validation class for get_all_books() endpoint'''
 class Response_For_Get_All_Books(BaseModel):
     success: str
-    api_key: str
     message: str
     books: List[AddBook]
