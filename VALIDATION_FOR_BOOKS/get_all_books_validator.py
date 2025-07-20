28d53df0c992ab9d4449a74d3b1e139b6e1da259
$_SUDOcode_44 <kwamecode11@gmail.com>
2025-07-20 15:42:50 -0700

    The validation folder for the USER and BOOKS endpoint was relocated to the USERS and BOOKS folder respectively. This was done to enable easier navigation of files.

--- VALIDATION_FOR_BOOKS/get_all_books_validator.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-from ENDPOINT_VALIDATIONS.add_book_validator import AddBook
-from typing import List
-
-'''The validation class for get_all_books() endpoint'''
-class Response_For_Get_All_Books(BaseModel):
-    success: str
-    message: str
-    books: List[AddBook]
