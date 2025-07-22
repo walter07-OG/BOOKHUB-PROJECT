cda398785076480a861aa7bf9b34a7325f64b11c
$_SUDOcode_44 <kwamecode11@gmail.com>
2025-07-21 17:43:04 -0700

    The validation class for the request was deleted because the request body is empty. We only access the Bearer token to create the blacklist. So we only create the validation class for the response.

--- USERS/VALIDATION_FOR_USER/user_logout.py
+++ USERS/VALIDATION_FOR_USER/user_logout.py
@@ -1,12 +1,5 @@
 from pydantic import BaseModel
 
-class Logout(BaseModel):
-    '''The validation class for logging out the user.(request)'''
-    #We initiate the get_api_token() function to extract the user's Bearer token to add it to
-    #the blacklist, preventing the user from making an additional request after logging out.
-
-
-
 class Logout_message(BaseModel):
     '''The validation for the success message after the user successfully logs out.'''
     success: bool
